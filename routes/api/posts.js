555cb26d0457be7b44672efcd755cd9f4ec62d5a
Carlos Aguirre <carlos.agu@gmail.com>
2018-08-23 14:19:26 -0600

    Posts started

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+
 
 router.get('/test', (req, res) => {
     res.json({
         msg: 'Posts Works'
     });
 });
+
+//@route POST api/posts
+//@desc     Create a Post
+//@access
 
 module.exports = router;
