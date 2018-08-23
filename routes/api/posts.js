9097433e7ebaeb902411ee3564c74761546614da
Carlos Aguirre <carlos.agu@gmail.com>
2018-08-23 15:01:42 -0600

    Post Validator

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,16 +3,29 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+const validatePostLogin = require('../../validation/post');
 
-
-router.get('/test', (req, res) => {
-    res.json({
-        msg: 'Posts Works'
-    });
-});
-
-//@route POST api/posts
+const Post = require('../../models/Post');
+//@route    POST api/posts
 //@desc     Create a Post
-//@access
+//@access   Private
+router.post('/', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const {
+        isValid,
+        errors
+    } = validatePostLogin(req.body);
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.name,
+        user: req.user.id
+    });
+    newPost.save().then(post => res.json(post))
+});
 
 module.exports = router;
